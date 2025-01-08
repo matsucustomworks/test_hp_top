af2a139f10aa134bba214d16e762830b159b9216
mcw <kenxnsr@gmail.com>
2025-01-08 17:38:16 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-// ナビゲーションメニューにスクロール時に動きをつける
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
+// ナビゲーションメニューの背景色変更
+window.onscroll = function() {
     var navbar = document.getElementById("navbar");
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        navbar.classList.add("navbar-active");
+        navbar.style.backgroundColor = "#333";  // スクロール後に背景色を変更
     } else {
-        navbar.classList.remove("navbar-active");
+        navbar.style.backgroundColor = "transparent";  // 元の状態に戻す
     }
-}
+};
